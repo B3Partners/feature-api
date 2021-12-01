8928e123359676f42ac271c1b77ff45449648ce9
mvdstruijk <m_struijk1990@hotmail.nl>
2021-12-01 16:51:43 +0100

    added POJO class for BulkUpdate request

--- src/main/java/nl/b3p/featureapi/resource/BulkUpdateBody.java
+++ src/main/java/nl/b3p/featureapi/resource/BulkUpdateBody.java
@@ -1,2 +1,25 @@
-package nl.b3p.featureapi.resource;public class BulkUpdateBody {
+package nl.b3p.featureapi.resource;
+
+import java.util.Map;
+
+public class BulkUpdateBody {
+    private String filter;
+
+    private Map<String, String> updatedFields;
+
+    public String getFilter() {
+        return filter;
+    }
+
+    public void setFilter(String filter) {
+        this.filter = filter;
+    }
+
+    public Map<String, String> getUpdatedFields() {
+        return updatedFields;
+    }
+
+    public void setUpdatedFields(Map<String, String> updatedFields) {
+        this.updatedFields = updatedFields;
+    }
 }
